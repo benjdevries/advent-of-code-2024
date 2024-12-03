c55e53d60c8962aacd807f4c5e78a166d521e00e
Ben DeVries <bdevries@edustaff.org>
2024-12-03 11:55:41 -0500

    complete day 3

---
+++ day_03/part2.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+def main():
+    fname = sys.argv[1]
+    instruction_match = re.compile(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
+    result = 0
+    enabled = True
+    
+    with open(fname) as f:
+        for line in f:
+            for instruction in instruction_match.finditer(line):
+                match instruction.group():
+                    case "do()":
+                        enabled = True
+                    case "don't()":
+                        enabled = False
+                    case _ if enabled:
+                        x, y = instruction.groups()
+                        result += int(x) * int(y)
+            
+    print(result)
+
+if __name__ == "__main__":
+    main()
