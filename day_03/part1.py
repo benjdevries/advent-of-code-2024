6fc7dbdee428aada7690b938e151dd38b807e536
Ben DeVries <bdevries@edustaff.org>
2024-12-04 12:53:10 -0500

    complete day 3

--- day_03/part1.py
+++ day_03/part1.py
@@ -1,18 +1,20 @@
 import re
 import sys
 
+
 def main():
     fname = sys.argv[1]
     instruction_match = re.compile(r"mul\((\d+),(\d+)\)")
     result = 0
-    
+
     with open(fname) as f:
         for line in f:
             for match in instruction_match.finditer(line):
                 x, y = match.groups()
-                result += int(x) * int(y)  
-            
+                result += int(x) * int(y)
+
     print(result)
+
 
 if __name__ == "__main__":
     main()
--- day_03/part2.py
+++ day_03/part2.py
@@ -1,12 +1,13 @@
 import re
 import sys
 
+
 def main():
     fname = sys.argv[1]
     instruction_match = re.compile(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
     result = 0
     enabled = True
-    
+
     with open(fname) as f:
         for line in f:
             for instruction in instruction_match.finditer(line):
@@ -18,8 +19,9 @@ def main():
                     case _ if enabled:
                         x, y = instruction.groups()
                         result += int(x) * int(y)
-            
+
     print(result)
+
 
 if __name__ == "__main__":
     main()
